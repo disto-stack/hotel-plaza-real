e28d11901409693b7e6aab5cb5b3cbffbfef4401
disto-stack <juan.ocampo.2501@gmail.com>
2022-01-03 14:49:25 -0500

    create dev configuration in webpack

--- webpack.common.js
+++ webpack.common.js
@@ -6,40 +6,58 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: {
-        index: './src/javascript/index.js',
-        about: './src/javascript/about.js',
-        gallery: './src/javascript/gallery.js',
-        contact: './src/javascript/contact.js'
+        index: path.join(__dirname, 'src', 'javascript', 'index.js'),
+        about: path.join(__dirname, 'src', 'javascript', 'about.js'),
+        gallery: path.join(__dirname, 'src', 'javascript', 'gallery.js'),
+        contact: path.join(__dirname, 'src', 'javascript', 'contact.js')
     },
     output: {
         filename: '[name].bundle.js',
-        path: `${__dirname}/dist/javascript`,
+        path: `${__dirname}/dist`,
         clean: true
     },
     module: {
         rules: [
+            { 
+                test: /\.handlebars$/, 
+                loader: "handlebars-loader",
+                options: {
+                    partialDirs: [
+                        path.join(__dirname, 'src', 'pages', 'partials')
+                    ]
+                }
+            },
             {
                 test: /\.(c|sc|sa)ss$/,
                 use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    }, 
-                    "css-loader", 
+                    "style-loader", 
+                    "css-loader",
                     "sass-loader"
                 ]
             }
+            
         ]
     },
     plugins: [
-        new HandlebarsWebpackPlugin({
-            entry: path.join(process.cwd(), 'src', '**', '*.hbs'),
-            output: path.join(process.cwd(), 'dist', '[path]','[name].html'),
-            partials: [
-                path.join(process.cwd(), 'src', 'pages', 'partials', '*.hbs')
-            ]
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.handlebars'),
+            filename: path.join(__dirname, 'dist', 'index.html'),
+            chunks: ['index']
         }),
-        new MiniCssExtractPlugin({
-            filename: '../main.css'
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'pages', 'about.handlebars'),
+            filename: path.join(__dirname, 'dist', 'pages', 'about.html'),
+            chunks: ['index', 'about']
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'pages', 'gallery.handlebars'),
+            filename: path.join(__dirname, 'dist', 'pages', 'gallery.html'),
+            chunks: ['index', 'gallery']
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'pages', 'contact.handlebars'),
+            filename: path.join(__dirname, 'dist', 'pages', 'contact.html'),
+            chunks: ['index', 'contact']
         })
     ]
 }
---
+++ webpack.dev.js
@@ -0,0 +1,20 @@
+const { merge } = require('webpack-merge')
+const path = require('path')
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: "inline-source-map",
+    target: "web",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 8080,
+        open: true,
+        hot: true,
+    },
+    optimization: {
+        runtimeChunk: 'single'
+    }
+})
